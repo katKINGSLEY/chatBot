fb484d444fbd4220f4ac2244d5c0ffec7f5f9434
jcy180000 <jcy180000@utdallas.edu>
2022-11-09 17:21:10 -0600

    renamed pytorch file

--- pytorch.py
+++ pytorch.py
@@ -663,7 +663,7 @@ clip = 50.0
 teacher_forcing_ratio = 1.0
 learning_rate = 0.0001
 decoder_learning_ratio = 5.0
-n_iteration = 4000
+n_iteration = 1000
 print_every = 1
 save_every = 500
 
@@ -703,8 +703,7 @@ encoder.eval()
 decoder.eval()
 
 import pickle
-pickle.dump(encoder, open("wb", "ml.p"))
-pickle.dump(decoder, open("wb", "ml.p"))
+pickle.dump((encoder, decoder), open("ml.p", "wb"))
 
 # Initialize search module
 searcher = GreedySearchDecoder(encoder, decoder)
